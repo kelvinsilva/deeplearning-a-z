97aa2d70a96bfd00fd45a10945f8b2c878696202
kelvin <kelvin@hacklabs.com>
2018-04-08 17:21:22 -0700

    adding folders

---
+++ Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template.py
@@ -0,0 +1,22 @@
+# Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into Training set and Test set
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_test  = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
